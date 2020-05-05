a55bd20822838cd97d1d5604cf838f63197b1c2b
Hoanh An <hoanhan101@gmail.com>
2020-05-05 14:34:56 -0400

    link to admin view

--- netbox_virtual_circuit_plugin/navigation.py
+++ netbox_virtual_circuit_plugin/navigation.py
@@ -5,7 +5,25 @@ from utilities.choices import ButtonColorChoices
 menu_items = (
     PluginMenuItem(
         link='plugins:netbox_virtual_circuit_plugin:list_virtual_circuits',
-        link_text='List all virtual circuits',
+        link_text='Virtual Circuits',
         permissions=[],
+        buttons=(
+            # Link to the admin view to add a virtual circuit if user has "add_virtualcircuit" permission.
+            PluginMenuButton(
+                link='admin:netbox_virtual_circuit_plugin_virtualcircuit_add',
+                title='Add a new virtual circuit',
+                icon_class='fa fa-plus',
+                color=ButtonColorChoices.GREEN,
+                permissions=['netbox_virtual_circuit_plugin.add_virtualcircuit']
+            ),
+            # Links to the admin view to assign a virtual circuit to a VLAN if user has the "add_vcvlan" permission.
+            PluginMenuButton(
+                link='admin:netbox_virtual_circuit_plugin_vcvlan_add',
+                title='Assign a virtual circuit to a VLAN',
+                icon_class='fa fa-plus',
+                color=ButtonColorChoices.BLUE,
+                permissions=['netbox_virtual_circuit_plugin.add_vcvlan']
+            ),
+        )
     ),
 )
