c233c6c2c476586fb298670fb5d1a754db8de611
Hoanh An <hoanhan101@gmail.com>
2020-05-05 00:00:36 -0400

    attach vlan view to single vc view

--- netbox_virtual_circuit_plugin/views.py
+++ netbox_virtual_circuit_plugin/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.views.generic import View
 
-from .models import VirtualCircuit
+from .models import VirtualCircuit, VCVLAN
 
 
 class ListVirtualCircuitsView(View):
@@ -20,6 +20,8 @@ class VirtualCircuitView(View):
     """
     def get(self, request, vcid):
         vc = get_object_or_404(VirtualCircuit.objects.filter(vcid=vcid))
+        vlans = get_list_or_404(VCVLAN.objects.filter(vc=vc))
         return render(request, 'netbox_virtual_circuit_plugin/virtual_circuit.html', {
             'virtual_circuit': vc,
+            'vlans': vlans,
         })
