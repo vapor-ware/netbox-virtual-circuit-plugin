00ac25214d6cf5a2803a0eb57716b365e42a3b14
Hoanh An <hoanhan101@gmail.com>
2020-05-06 10:20:45 -0400

    serialize vcvlan

--- netbox_virtual_circuit_plugin/api/serializers.py
+++ netbox_virtual_circuit_plugin/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from netbox_virtual_circuit_plugin.models import VirtualCircuit
+from netbox_virtual_circuit_plugin.models import VirtualCircuit, VCVLAN
 
 class VirtualCircuitSerializer(ModelSerializer):
 
     class Meta:
         model = VirtualCircuit
+        fields = '__all__'
+
+class VCVLANSerializer(ModelSerializer):
+
+    class Meta:
+        model = VCVLAN
         fields = '__all__'
--- netbox_virtual_circuit_plugin/api/views.py
+++ netbox_virtual_circuit_plugin/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-from netbox_virtual_circuit_plugin.models import VirtualCircuit
-from .serializers import VirtualCircuitSerializer
+from netbox_virtual_circuit_plugin.models import VirtualCircuit, VCVLAN
+from .serializers import VirtualCircuitSerializer, VCVLANSerializer
 
 class VirtualCircuitViewSet(ModelViewSet):
     queryset = VirtualCircuit.objects.all()
     serializer_class = VirtualCircuitSerializer
+
+class VCVLANViewSet(ModelViewSet):
+    queryset = VCVLAN.objects.all()
+    serializer_class = VCVLANSerializer
