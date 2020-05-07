5754babde5ae0da4fcef44fe8769b80794763111
Hoanh An <hoanhan101@gmail.com>
2020-05-07 10:59:49 -0400

    update license to use GPLv3

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='netbox-virtual-circuit-plugin',
-    version='0.1',
+    version='0.1.0',
     description='A Netbox plugin that supports Virtual Circuit',
     url='https://github.com/vapor-ware/netbox-virtual-circuit-plugin',
     author='Hoanh An',
-    license='Apache 2.0',
+    license='GNU General Public License v3.0',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
