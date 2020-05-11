90cc6a0520da28dc1070f3186ea178cbea4d2a4f
Hoanh An <hoanhan101@gmail.com>
2020-05-11 16:26:08 -0400

    use README as project long description

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
+import os
 from setuptools import setup, find_packages
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = ""
+if os.path.exists('README.md'):
+    with open('README.md', 'r') as f:
+        readme = f.read()
 
 setup(
     name='netbox-virtual-circuit-plugin',
     version='0.1.0',
     description='A Netbox plugin that supports Virtual Circuit',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/vapor-ware/netbox-virtual-circuit-plugin',
     author='Hoanh An',
     license='GNU General Public License v3.0',
+    package_data={
+        '': ['LICENSE'],
+    },
     install_requires=[],
     packages=find_packages(),
-    include_package_data=True,
+    include_package_data=True
 )
