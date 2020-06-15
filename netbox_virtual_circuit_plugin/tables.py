b42a1c43d1b903ae0280644344169a8fbf3a126b
Hoanh An <hoanhan101@gmail.com>
2020-06-15 00:03:34 -0400

    link to vc detailed view

--- netbox_virtual_circuit_plugin/tables.py
+++ netbox_virtual_circuit_plugin/tables.py
@@ -1,4 +1,5 @@
 import django_tables2 as tables
+from django_tables2.utils import Accessor
 from utilities.tables import BaseTable, ToggleColumn
 
 from .models import VirtualCircuit
@@ -6,6 +7,10 @@ from .models import VirtualCircuit
 
 class VirtualCircuitTable(BaseTable):
     pk = ToggleColumn()
+    vcid = tables.LinkColumn(
+        viewname='plugins:netbox_virtual_circuit_plugin:virtual_circuit',
+        args=[Accessor('vcid')]
+    )
 
     class Meta(BaseTable.Meta):
         model = VirtualCircuit
