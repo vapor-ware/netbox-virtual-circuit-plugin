8095a5f7d86ec47cd808dafef5e359bb826adced
Hoanh An <hoanhan101@gmail.com>
2020-07-20 17:38:17 -0400

    add tests

--- netbox_virtual_circuit_plugin/tests/test_models.py
+++ netbox_virtual_circuit_plugin/tests/test_models.py
@@ -15,11 +15,21 @@ class VirtualCircuitTestCase(TestCase):
 
         self.vc1_vlan1 = VirtualCircuitVLAN.objects.create(virtual_circuit=self.vc1, vlan=self.vlan1)
 
+    def test_invalid_vcid(self):
+        negative = VirtualCircuit(vcid=-1, name='VC -1')
+        self.assertRaises(ValidationError, negative.full_clean)
+
+        zero = VirtualCircuit(vcid=0, name='VC 0')
+        self.assertRaises(ValidationError, zero.full_clean)
+
+        out_of_range = VirtualCircuit(vcid=4294967296, name='VC 4294967296')
+        self.assertRaises(ValidationError, out_of_range.full_clean)
+
     def test_duplicate_vlan(self):
         duplicate = VirtualCircuitVLAN(virtual_circuit=self.vc1, vlan=self.vlan1)
         self.assertRaises(ValidationError, duplicate.full_clean)
 
-    def test_create_vlan(self):
+    def test_create_virtual_circuit(self):
         vc1_vlan2 = VirtualCircuitVLAN.objects.create(virtual_circuit=self.vc1, vlan=self.vlan2)
         self.assertEqual(vc1_vlan2.virtual_circuit, self.vc1)
         self.assertEqual(vc1_vlan2.vlan, self.vlan2)
