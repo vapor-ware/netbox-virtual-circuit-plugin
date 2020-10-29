4ce55ca3a726cec81e57f41019d2b0682ef8b820
Hoanh An <hoanhan101@gmail.com>
2020-10-29 17:17:52 -0400

    clean up vlans

--- netbox_virtual_circuit_plugin/template_content.py
+++ netbox_virtual_circuit_plugin/template_content.py
@@ -8,16 +8,19 @@ class TenantVirtualCircuitCount(PluginTemplateExtension):
     model = 'tenancy.tenant'
 
     def right_page(self):
+        # Map Virtual Circuits' IDs to VLANs' IDs.
         m = {}
+
+        # Filter VLANs by tenant.
         vlans = VLAN.objects.filter(tenant=self.context['object'])
         for v in vlans:
+            # If a VLAN is presented, map it back to its virtual circuit to prevent duplicates.
             if VirtualCircuitVLAN.objects.filter(vlan=v).count() == 1:
                 vcvlan = VirtualCircuitVLAN.objects.get(vlan=v)
                 m[vcvlan.virtual_circuit.vcid] = vcvlan.vlan.id
 
         return self.render('netbox_virtual_circuit_plugin/virtual_circuit_tenant.html', extra_context={
             'count': len(m),
-            'vlan_count': vlans.count(),
         })
 
 
